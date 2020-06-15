62c3c2a742749e81b9a44ebd327ee9d3a3a6f968
Nikhilpavan944 <49267107+Nikhilpavan944@users.noreply.github.com>
2020-06-15 20:35:03 +0530

    Added Test Cases

--- src/main/java/com/example/Temporary/service_impl/SizesImpl.java
+++ src/main/java/com/example/Temporary/service_impl/SizesImpl.java
@@ -11,50 +11,51 @@ import java.util.List;
 @Service
 public class SizesImpl implements ISize {
 
-    @Autowired
-    SizesDao sizesDao;
+	@Autowired
+	SizesDao sizesDao;
 
+	@Override
+	public Sizes createSize(SizesDTO sizesDTO) {
 
-    @Override
-    public Sizes createSize(SizesDTO sizesDTO) {
+		Sizes sizes = new Sizes();
 
-        Sizes sizes = new Sizes();
+		sizes.setSizeName(sizesDTO.getSizeName());
 
-        sizes.setSizeName(sizesDTO.getSizeName());
+		sizes.setSizePrice(sizesDTO.getSizePrice());
 
-        sizes.setSizePrice(sizesDTO.getSizePrice());
+		//Sizes sizeObjs = sizesDao.save(sizes);
+		
+		return sizesDao.save(sizes);
+	}
 
-        return sizesDao.save(sizes);
-    }
+	@Override
+	public Sizes updateSize(Long id, SizesDTO sizesDTO) {
 
-    @Override
-    public Sizes updateSize(Long id, SizesDTO sizesDTO) {
+		Sizes sizes = sizesDao.getOne(id);
 
-        Sizes sizes = sizesDao.getOne(id);
+		sizes.setSizeName(sizesDTO.getSizeName());
 
-        sizes.setSizeName(sizesDTO.getSizeName());
+		sizes.setSizePrice(sizesDTO.getSizePrice());
 
-        sizes.setSizePrice(sizesDTO.getSizePrice());
+		return sizesDao.save(sizes);
+	}
 
-        return sizesDao.save(sizes);
-    }
+	@Override
+	public String deleteSize(Long id) {
 
-    @Override
-    public String deleteSize(Long id) {
+		sizesDao.deleteById(id);
 
-        sizesDao.deleteById(id);
+		return "Successfully Deleted Size with id:" + id;
+	}
 
-        return "Successfully Deleted Size with id:"+id;
-    }
+	@Override
+	public Sizes getOne(Long id) {
+		return sizesDao.getOne(id);
+	}
 
-    @Override
-    public Sizes getOne(Long id) {
-        return sizesDao.getOne(id);
-    }
-
-    @Override
-    public List<Sizes> getAll() {
-        return sizesDao.findAll();
-    }
+	@Override
+	public List<Sizes> getAll() {
+		return sizesDao.findAll();
+	}
 
 }
